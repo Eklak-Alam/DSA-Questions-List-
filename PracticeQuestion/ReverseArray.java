b073b481b3cd9b3d0714f74f8ff824692a7c7a1a
Eklak Alam <eklakalam420@gmail.com>
2025-06-12 10:25:24 +0530

    added question

---
+++ PracticeQuestion/ReverseArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int n = arr.length;
+
+        //Method 1
+        int start = 0;
+        int end = n - 1;
+
+        while (start< end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        //method 2
+        for(int i = 0; i < n/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }    
+}
